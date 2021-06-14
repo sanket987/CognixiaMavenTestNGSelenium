4d2e787c03bef43d30225f0dddcea6e3fdc06fed
dqauser1 <ameya.naik+dqauser1@gmail.com>
2021-06-14 13:43:26 +0530

    Day 11 - Lunch

--- src/test/java/com/cognixia/training/MavenTestNGSelenium/tests/TestNGExample.java
+++ src/test/java/com/cognixia/training/MavenTestNGSelenium/tests/TestNGExample.java
@@ -1,5 +1,6 @@
 package com.cognixia.training.MavenTestNGSelenium.tests;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -8,9 +9,18 @@ import org.testng.annotations.Test;
 
 public class TestNGExample {
 	
+	private String name;
+	
 	@Test
 	public void testFirst() {
 		System.out.println("Inside test First");
+		getData(); //Calling the method or executing the method
+		name = "Ameya";
+		
+	}
+	
+	public void getData() { //Declaring the method
+		System.out.println("Inside Get Data");
 	}
 	
 	@BeforeTest
@@ -26,6 +36,7 @@ public class TestNGExample {
 	@Test
 	public void testSecond() {
 		System.out.println("Inside test Second");
+		System.out.println("Name is: "+name);
 	}
 	
 	@BeforeMethod
@@ -37,5 +48,15 @@ public class TestNGExample {
 	public void beforeSuite() {
 		System.out.println("Inside Before Suite");
 	}
-
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Inside After Suite");
+	}
+	
+	@Test
+	public void testA() {
+		System.out.println("Inside test A");
+		System.out.println("Name is: "+name);
+	}
 }
